a39a2155a5753d22cd776035bba9aac6a7b98145
Eric Raguzin <eraguzin@bnl.gov>
2022-03-07 21:34:42 -0500

    window works

--- spectrum.py
+++ spectrum.py
@@ -3,25 +3,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fft import fft, fftfreq
+import pandas as pd
+from scipy.signal import blackman
 
-# Number of sample points
+class ADC():
+    def __init__(self, input_file):
 
-N = 600
+        df = pd.read_csv(input_file, engine='python', usecols=[0,1])
+        print(df)
 
-# sample spacing
+        df_info=pd.read_csv(input_file, engine='python', usecols=[3,4], names=[0,1], nrows=11)
+        print(df_info)
+        samples = int(df_info.iat[4,1])
+        sample_rate = int(df_info.iat[3,1]) * 1e6
 
-T = 1.0 / 800.0
+        print(samples)
+        print(sample_rate)
 
-x = np.linspace(0.0, N*T, N, endpoint=False)
 
-y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
+        # Number of sample points
+        N = samples
+        w = blackman(N)
+        # sample spacing
 
-yf = fft(y)
+        T = 1.0 / sample_rate
 
-xf = fftfreq(N, T)[:N//2]
+        x = np.linspace(0.0, N*T, N, endpoint=False)
 
-plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+        y = df['Magnitude'].tolist()
+        print(y)
+        print(type(y))
+#        y = pd.DataFrame([0,1,2])
+#        print(y)
+#        print(type(y))
 
-plt.grid()
+        yf = fft(y)
+        ywf = fft(y*w)
+        xf = fftfreq(N, T)[:N//2]
 
-plt.show()
+        plt.semilogy(xf, 2.0/N * np.abs(yf[0:N//2]))
+        plt.semilogy(xf, 2.0/N * np.abs(ywf[0:N//2]))
+
+        plt.grid()
+
+        plt.show()
+
+if __name__ == "__main__":
+    ADC("ad9253_outputs/50MHz/samples.csv")
